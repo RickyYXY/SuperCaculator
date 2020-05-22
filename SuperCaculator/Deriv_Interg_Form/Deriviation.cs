0daf0f96c5ce35d5c342567a4db7fdadf3cff530
RickyYXY <yuxingyuan@gmail.com>
2020-05-22 18:35:07 +0800

    微分计算类

---
+++ SuperCaculator/Deriv_Interg_Form/Deriviation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerivIntergForm
+{
+    class Deriviation
+    {
+        private Function.Function function;
+        private double precision;
+        public Deriviation(double pre)
+        {
+            precision = pre;
+        }
+        public double DeriviCal(double x,string exp)
+        {
+            function = new Function.Function(exp);
+            double up = function.GetValue(x + precision);
+            double down = function.GetValue(x - precision);
+            return (up - down) / (2 * precision);
+        }
+
+    }
+}
