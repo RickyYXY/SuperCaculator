71aeebc4695764c3fbbd4fa426ddab888c0bbc82
wycwcq <1193032407@qq.com>
2020-05-25 11:09:58 +0800

    添加了MatrixCalculateForm模块

---
+++ SuperCaculator/MatrixCalculateForm/MatrixCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixCalculateForm
+{
+    class MatrixCreate : Matrix
+    {
+        public MatrixCreate(int rowNum, int columnNum) : base(rowNum, columnNum)
+        {
+            MatrixRow = rowNum;
+            MatrixColumn = columnNum;
+            ValueNumbers = new int[MatrixRow, MatrixColumn];
+        }
+
+        public void ZerosMatrix()
+        {
+            for(int i = 0; i < MatrixRow; i++)
+            {
+                for(int j=0; j<MatrixColumn; j++)
+                {
+                    ValueNumbers[i, j] = 0;
+                }
+            }
+        }
+
+        public void IdentityMatrix()
+        {
+            int temp = 0;
+            for(int i = 0; i < MatrixRow; i++)
+            {
+                for(int j =0;j<MatrixColumn; j++)
+                {
+                    if(i == (j + temp))
+                    {
+                        ValueNumbers[i, j] = 1;
+                    }
+                    else
+                    {
+                        ValueNumbers[i, j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
