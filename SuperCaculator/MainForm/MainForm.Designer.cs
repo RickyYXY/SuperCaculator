29e2ef1ab3993ce74959148181796fc49bb1d8a9
Shyuna <1229324428@qq.com>
2020-06-08 22:21:34 +0800

    修改主界面目录的字体

--- SuperCaculator/MainForm/MainForm.Designer.cs
+++ SuperCaculator/MainForm/MainForm.Designer.cs
@@ -59,10 +59,10 @@
             this.panel1.Controls.Add(this.treeView1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(10, 10, 10, 10);
-            this.panel1.Size = new System.Drawing.Size(1640, 611);
+            this.panel1.Padding = new System.Windows.Forms.Padding(12, 11, 12, 11);
+            this.panel1.Size = new System.Drawing.Size(1924, 645);
             this.panel1.TabIndex = 1;
             // 
             // panelChild
@@ -71,10 +71,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelChild.BackColor = System.Drawing.Color.AliceBlue;
-            this.panelChild.Location = new System.Drawing.Point(272, 12);
-            this.panelChild.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelChild.Location = new System.Drawing.Point(332, 13);
+            this.panelChild.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.panelChild.Name = "panelChild";
-            this.panelChild.Size = new System.Drawing.Size(1354, 587);
+            this.panelChild.Size = new System.Drawing.Size(1575, 620);
             this.panelChild.TabIndex = 1;
             // 
             // treeView1
@@ -82,9 +82,9 @@
             this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.treeView1.BackColor = System.Drawing.Color.AliceBlue;
-            this.treeView1.Font = new System.Drawing.Font("仿宋", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.treeView1.Location = new System.Drawing.Point(14, 12);
-            this.treeView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.treeView1.Font = new System.Drawing.Font("方正舒体", 21F);
+            this.treeView1.Location = new System.Drawing.Point(17, 13);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.treeView1.Name = "treeView1";
             treeNode1.Name = "standard";
             treeNode1.Text = "标准";
@@ -109,17 +109,18 @@
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode6,
             treeNode10});
-            this.treeView1.Size = new System.Drawing.Size(253, 586);
+            this.treeView1.Size = new System.Drawing.Size(308, 618);
             this.treeView1.TabIndex = 0;
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.MenuClick);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1640, 611);
+            this.ClientSize = new System.Drawing.Size(1924, 645);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Font = new System.Drawing.Font("方正舒体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.Name = "MainForm";
             this.Text = "多功能计算器";
             this.Load += new System.EventHandler(this.MainForm_Load);
