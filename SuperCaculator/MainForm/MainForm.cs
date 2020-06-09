141d2042f91155e522cbb2ee281994b2af013419
Shyuna <1229324428@qq.com>
2020-06-09 17:45:31 +0800

    删除不必要的using

--- SuperCaculator/MainForm/MainForm.cs
+++ SuperCaculator/MainForm/MainForm.cs
@@ -3,14 +3,6 @@ using MatrixCalculateForm;
 using PaintForm;
 using StandardCalculateForm;
 using System;
-using EquationsSolvingModule;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MainForm
